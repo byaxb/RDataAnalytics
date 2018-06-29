c78ece9fae25605653685f14eac040a5dd562558
byaxb <axb@bupt.edu.cn>
2018-06-29 16:31:59 +0800

    Update ch07_Cluster Analysis.R

--- ch07_Cluster Analysis.R
+++ ch07_Cluster Analysis.R
@@ -6,12 +6,12 @@
 ## 名称：《R语言数据分析·聚类分析》
 ## 作者：艾新波
 ## 学校：北京邮电大学
-## 版本：V7
-## 时间：2017年9月
+## 版本：V8
+## 时间：2018年3月
 ##
 ##*****************************************************
 ##
-## ch07_Cluster Analysis_V7
+## ch07_Cluster Analysis_V8
 ## Data Analytics with R
 ## Instructed by Xinbo Ai
 ## Beijing University of Posts and Telecommunications
@@ -26,7 +26,7 @@
 ##
 ##*****************************************************
 ##
-## (c)2012~2017
+## (c)2012~2018
 ##
 #######################################################
 #######################################################
@@ -47,8 +47,18 @@
 #######################################################
 #加载数据
 rm(list = ls())
-load("cj.rda", verbose = TRUE)
-View(cheng_ji_biao)
+library(tidyverse)
+library(magrittr)
+library(GGally)
+cjb_url <-"https://github.com/byaxb/RDataAnalytics/raw/master/data/cjb.csv"
+
+cjb <- read_csv(cjb_url,
+                locale = locale(encoding = "CP936"))
+cjb %<>%
+  mutate(zcj = rowSums(.[4:12])) %>%
+  #filter(zcj != 0) %>%#剔除脏数据
+  mutate_at(vars(xb, wlfk), factor) #类型转换
+
 
 #对于聚类而言，主要是观察数据空间的结构
 #这里的空间结构，主要就是距离结构
@@ -58,9 +68,9 @@ View(cheng_ji_biao)
 #平行坐标图
 #结果与Get to Know Your Data一样
 #但是采用pipe方式重写了
-cheng_ji_biao %>%
-  group_by(文理分科) %>%
-  arrange(desc(总成绩)) %>%
+cjb %>%
+  group_by(wlfk) %>%
+  arrange(desc(zcj)) %>%
   head(n = 300) %>%
   ungroup() %>%
   ggparcoord(columns = 3:12,
@@ -70,23 +80,44 @@ cheng_ji_biao %>%
 #但更应观测其中具有辨识能力的变量
 #如生物、物理、数学等
 
+raw_cj <-  cjb%>%
+  group_by(wlfk) %>%
+  arrange(desc(zcj)) %>%
+  head(n = 300) %>%
+  ungroup()
+cj_jittered <- as.data.frame(cjb)
+for(j in 3:12) {
+  cj_jittered[, j] <- jitter(as.numeric(cj_jittered[, j]))
+}
+cj_jittered %>%
+  group_by(wlfk) %>%
+  arrange(desc(zcj)) %>%
+  head(n = 300) %>%
+  ungroup() %>%
+  ggparcoord(columns = 3:12,
+             alpha = 0.5, 
+             groupColumn = 13) +
+  geom_point(size = 1.2)
+
+
+
 #聚类，本质上是识别数据点在数据空间的（距离）结构
 #可以先看看在生物、物理二维空间里是怎么分布的
 library(ggplot2)
-ggplot(cheng_ji_biao, aes(x = 物理, y = 数学)) +
-  geom_point(aes(colour = 文理分科, shape = 文理分科))
+ggplot(cjb, aes(x = wl, y = sx)) +
+  geom_point(aes(colour = wlfk, shape = wlfk))
 #数据空间的密度
 #将50~100细分为N份，看每一个有多少落入其间
 ibreaks <- seq(50, 100, len = 21)
-cheng_ji_biao %>%
-  select(物理, 数学) %>%
-  mutate(物理 = cut(物理, breaks = ibreaks),
-           数学 = cut(数学, breaks = ibreaks)) %>%
-  group_by(物理, 数学) %>%
+cjb %>%
+  select(wl, sx) %>%
+  mutate(wl = cut(wl, breaks = ibreaks),
+           sx = cut(sx, breaks = ibreaks)) %>%
+  group_by(wl, sx) %>%
   summarise(freq = n()) %>%
-  complete(物理, 数学) %>%
+  complete(wl, sx) %>%
   mutate(freq = ifelse(is.na(freq), 0, freq)) %>%
-  ggplot(aes(x = 物理, y = 数学, fill = freq)) +
+  ggplot(aes(x = wl, y = sx, fill = freq)) +
   geom_tile(colour="white", size = 0.5) +
   geom_text(aes(label = freq), size = 3) +
   scale_fill_gradient(low = "white", high = "red")+
@@ -96,40 +127,40 @@ cheng_ji_biao %>%
 #三维散点图
 library(rgl)
 plot3d(
-  x = cheng_ji_biao$数学,
-  y = cheng_ji_biao$物理,
-  z = cheng_ji_biao$生物,
+  x = cjb$sx,
+  y = cjb$wl,
+  z = cjb$sw,
   xlab = "Mathematics", 
   ylab = "Physics",
   zlab = "Biology",
   type = "s",
   size = 0.5,
-  col = c("red", "green")[cheng_ji_biao$文理分科])
+  col = c("red", "green")[cjb$wlfk])
 
 #剔除其中的缺失值和异常值
-na_idx <- which(cheng_ji_biao$总成绩 == 0)
-cheng_ji_biao <- cheng_ji_biao[-na_idx, ]
-(outliers <- boxplot.stats(cheng_ji_biao$总成绩)$out)
+na_idx <- which(cjb$zcj == 0)
+cjb <- cjb[-na_idx, ]
+(outliers <- boxplot.stats(cjb$zcj)$out)
 #离群值标签
-(outliers_idx <- which(cheng_ji_biao$总成绩
+(outliers_idx <- which(cjb$zcj
                        %in% outliers))
-cheng_ji_biao <- cheng_ji_biao[-outliers_idx, ]
+cjb <- cjb[-outliers_idx, ]
 #重新运行前述plot3d代码
 library(rgl)
 plot3d(
-  x = cheng_ji_biao$数学,
-  y = cheng_ji_biao$物理,
-  z = cheng_ji_biao$生物,
+  x = cjb$sx,
+  y = cjb$wl,
+  z = cjb$sw,
   xlab = "Mathematics", 
   ylab = "Physics",
   zlab = "Biology",
   type = "s",
   radius = 0.5,
-  col = c("red", "green")[cheng_ji_biao$文理分科])
+  col = c("red", "green")[cjb$wlfk])
 
 #绘制其中的密度分布
-selected_cols <- c("数学", "物理", "生物")
-shu_wu_sheng <- cheng_ji_biao[, selected_cols]
+selected_cols <- c("sx", "wl", "sw")
+shu_wu_sheng <- cjb[, selected_cols]
 sws_dist <- as.matrix(dist(shu_wu_sheng,
                            diag = TRUE,
                            upper = TRUE))
@@ -164,7 +195,7 @@ for(i in seq(nrow(grd))){
   #设定长宽高
   icube3d <- scale3d(icube3d, length, width, height)
   #将长方体移动至指定位置
-  icube3d <- translate3d(icube3d, grd$数学[i], grd$物理[i], grd$生物[i])
+  icube3d <- translate3d(icube3d, grd$sx[i], grd$wl[i], grd$sw[i])
   #绘制长方体
   shade3d(icube3d, alpha = 0.05)
 }
@@ -179,17 +210,25 @@ for(i in seq(nrow(grd))){
 
 #数据能聚类么？
 #霍普金斯统计量
+set.seed(2012)
+cj_matri <- cjb %>%
+  select(yw:sw) %>%
+  as.matrix()
 clustertend::hopkins(cj_matri, n = 100)
-#[1] 0.1865712
+#> $`H`
+#> [1] 0.2087974
 #取值偏向于0.5时，是均匀的
 #偏向于0时，是倾斜的
 
 #当然，我们也可以同时取多个值
-sapply((1:10)*50, function(x) {
+hopkins_metrics <- sapply((1:10)*50, function(x) {
   clustertend::hopkins(cj_matri, x)
 })
-# 0.2123391 0.1990008 0.1992889 0.204042 0.1974034 
-#0.1920272 0.2077448 0.191369 0.193734 0.1989523
+unlist(hopkins_metrics)
+#>0.2086024 0.2020664 0.1915297 0.1859097 0.1997344 
+#>0.2050931 0.2071697 0.1960066 0.1915444 0.2002765 
+mean(unlist(hopkins_metrics))
+#> [1] 0.1987933
 #由此可见，目前这份数据进行聚类还是可行的
 
 
@@ -236,7 +275,7 @@ as.matrix(daisy(artificial_data))
 #若只考虑各科成绩，
 #观察各数据点（一个同学一个数据点）
 #之间的距离
-cj <- cheng_ji_biao[, 4:12]
+cj <- cjb[, 4:12]
 cj_matri <- as.matrix(cj)
 cj_dist <- dist(cj_matri)
 #距离可视化
@@ -251,7 +290,7 @@ fviz_dist(
 )
 #也可以考虑性别
 library(cluster)
-cj_dist_plus <- daisy(cheng_ji_biao[, 3:12])
+cj_dist_plus <- daisy(cjb[, 3:12])
 fviz_dist(
   cj_dist_plus,
   order = FALSE,
@@ -267,8 +306,8 @@ cj_mds <- cmdscale(d = cj_dist, k = 2)
 library(tidyverse)
 cj_mds %>%
   as.data.frame() %>%
-  mutate(name = cheng_ji_biao$姓名,
-         type = cheng_ji_biao$文理分科) %>%
+  mutate(name = cjb$xm,
+         type = cjb$wlfk) %>%
   setNames(c("x", "y", "name", "type")) %>%
   ggplot(aes(x = x, y = y)) + 
   geom_text(aes(label = name, colour = type), 
@@ -278,9 +317,9 @@ cj_mds_plus <- cmdscale(d = cj_dist_plus, k = 2)
 library(tidyverse)
 cj_mds_plus %>%
   as.data.frame() %>%
-  mutate(name = cheng_ji_biao$姓名,
-         type = cheng_ji_biao$文理分科,
-         sex = cheng_ji_biao$性别) %>%
+  mutate(name = cjb$xm,
+         type = cjb$wlfk,
+         sex = cjb$xb) %>%
   setNames(c("x", "y", "name", "type", "sex")) %>%
   ggplot(aes(x = x, y = y)) + 
   geom_text(aes(label = name, colour = type), 
@@ -289,9 +328,9 @@ cj_mds_plus %>%
 #莫非增加性别之后就把他们区别开来了？
 cj_mds_plus %>%
   as.data.frame() %>%
-  mutate(name = cheng_ji_biao$姓名,
-         type = cheng_ji_biao$文理分科,
-         sex = cheng_ji_biao$性别) %>%
+  mutate(name = cjb$xm,
+         type = cjb$wlfk,
+         sex = cjb$xb) %>%
   setNames(c("x", "y", "name", "type", "sex")) %>%
   ggplot(aes(x = x, y = y)) + 
   geom_text(aes(label = name, colour = sex), 
@@ -299,30 +338,121 @@ cj_mds_plus %>%
 #由此可见，数据空间，并没有很好地区分为文理科两类
 
 
+#主成分分析
+library(stats)
+
+cj_pc <- princomp(cjb[, 4:12])
+cj_pc <- prcomp(cjb[, 4:12])
+str(cj_pc)
+cj_pc$sdev[1] / sum(cj_pc$sdev)
+cj_pc$sdev[2] / sum(cj_pc$sdev)
+
+biplot(cj_pc)
+
+str(cj_pc$loadings)
+cj_reduced <- predict(cj_pc, cjb[, 4:12])
+cj_reduced <- cj_reduced %>%
+  as.data.frame() %>%
+  mutate(type = cjb$wlfk)
+ggplot(cj_reduced, aes(x = PC1, y = PC2, colour = type)) +
+  geom_point()
 
 
 #######################################################
 ##k-means算法
 #######################################################
 
-#注意区别是否剔除异常点的区别
-# load("cj.rda")
-# #剔除其中的缺失值和异常值
-# na_idx <- which(cheng_ji_biao$总成绩 == 0)
-# cheng_ji_biao <- cheng_ji_biao[-na_idx, ]
-# (outliers <- boxplot.stats(cheng_ji_biao$总成绩)$out)
-# #离群值标签
-# (outliers_idx <- which(cheng_ji_biao$总成绩
-#                        %in% outliers))
-# cheng_ji_biao <- cheng_ji_biao[-outliers_idx, ]
 
-cj <- cheng_ji_biao[, 4:12]
+#先直观展示一下kmeans的迭代过程
+#小伙伴们也可以如法炮制
+#写一些简单版的算法，有助于理解算法原理
+library(tidyverse)
+library(animation)
+library(deldir)
+saveGIF(
+  expr = {
+    data("iris")
+    set.seed(2012)
+    init_centers_idx <- sample(150, 3)
+    init_centers <-
+      iris[init_centers_idx, c("Petal.Length", "Petal.Width")]
+    voronoi <- deldir(init_centers[, 1], init_centers[, 2]) #对空间进行划分
+    cur_centers <- init_centers
+    old_centers <- -cur_centers #其实可以是和cur_centers长宽相同的任意data.frame,但里边的值不能相同
+    old_centers == cur_centers
+    repeat_first <- TRUE
+    while (any(old_centers != cur_centers)) { #新的中心和原来的中心是否完全一样
+      #将点分派至不同的中心
+      Species_type <- apply(iris[, c("Petal.Length", "Petal.Width")], 1, function(x) {
+        which.min(apply(cur_centers[, c("Petal.Length", "Petal.Width")], 1, function(y) {
+          sqrt(sum((x - y) ^ 2))
+        }))
+      })
+      iris$Species <-
+        c("setosa", "versicolor", "virginica")[Species_type]
+      voronoi <-
+        deldir(cur_centers$Petal.Length, cur_centers$Petal.Width)
+      p <- ggplot() +
+        geom_point(data = iris, 
+                   aes(x = Petal.Length, 
+                       y = Petal.Width, 
+                       colour = Species)) +
+        geom_point(
+          data = cur_centers,
+          aes(x = Petal.Length,
+              y = Petal.Width,
+              colour = "red"),
+          size = 3
+        ) +
+        geom_segment(
+          data = voronoi$dirsgs,
+          aes(
+            x = x1,
+            y = y1,
+            xend = x2,
+            yend = y2
+          ),
+          size = 1.2,
+          linetype = 1,
+          color = "red"
+        ) +
+        coord_fixed()+ #固定长宽比例，否则看不出垂直的效果
+        theme(legend.position = "none")
+      
+      plot(p)
+      if(repeat_first) {
+        plot(p)
+        plot(p)
+        plot(p)
+        repeat_first <- FALSE
+      }
+      
+      
+      old_centers <- cur_centers
+      #重新计算各组的中心
+      cur_centers <- iris %>%
+        group_by(Species) %>%
+        summarise(Petal.Length = mean(Petal.Length),
+                  Petal.Width = mean(Petal.Width)) %>%
+        mutate(Species = NULL)
+    }
+  },
+  movie.name = "animation.gif",
+  convert = "gm convert",
+  interval = 1,
+  ani.width = diff(range(iris$Petal.Length))*100,
+  ani.height = diff(range(iris$Petal.Width))*100
+)
+
+
+
+cj <- cjb[, 4:12]
 #stats包中的kmeans()函数
 set.seed(2012)
 imodel <- kmeans(cj, centers = 2, iter.max = 5000)
-min(Metrics::ce(cheng_ji_biao$文理分科, 
+min(Metrics::ce(cjb$wlfk, 
                 c( "理科", "文科")[imodel$cluster]),
-    1- Metrics::ce(cheng_ji_biao$文理分科, 
+    1- Metrics::ce(cjb$wlfk, 
                    c( "理科", "文科")[imodel$cluster]))
 #[1] 0.36 未剔除异常点
 #[1] 0.3464567 剔除异常点
@@ -336,11 +466,11 @@ factoextra::fviz_cluster(imodel,
 #是特征越多越好么？
 #未必如此
 set.seed(2012)
-imodel <- kmeans(cj[, c("生物", "物理", "数学")], centers = 2, iter.max = 5000)
-table(cheng_ji_biao$文理分科, c("文科", "理科")[imodel$cluster])
-min(Metrics::ce(cheng_ji_biao$文理分科, 
+imodel <- kmeans(cj[, c("sw", "wl", "sx")], centers = 2, iter.max = 5000)
+table(cjb$wlfk, c("文科", "理科")[imodel$cluster])
+min(Metrics::ce(cjb$wlfk, 
                 c( "理科", "文科")[imodel$cluster]),
-    1- Metrics::ce(cheng_ji_biao$文理分科, 
+    1- Metrics::ce(cjb$wlfk, 
                    c( "理科", "文科")[imodel$cluster]))
 #[1] 0.3277419未剔除异常点
 #[1] 0.3228346 剔除异常点
@@ -348,13 +478,13 @@ min(Metrics::ce(cheng_ji_biao$文理分科,
 library(flexclust)
 mycont = list(iter=5000, initcent = "kmeanspp")
 as(mycont, "flexclustControl")
-imodel_kmeans2 <- kcca(cj[, c("生物", "物理", "数学")], 
+imodel_kmeans2 <- kcca(cj[, c("sw", "wl", "sx")], 
                        k = 2, 
                        family=kccaFamily("kmeans"),
                        control = mycont)
-min(Metrics::ce(cheng_ji_biao$文理分科, 
+min(Metrics::ce(cjb$wlfk, 
             c( "理科", "文科")[imodel_kmeans2@cluster]),
-    1- Metrics::ce(cheng_ji_biao$文理分科, 
+    1- Metrics::ce(cjb$wlfk, 
                    c( "理科", "文科")[imodel_kmeans2@cluster]))
 #[1] 0.3303226 未剔除异常点
 #[1] 0.312336 剔除异常点
@@ -366,17 +496,17 @@ min(Metrics::ce(cheng_ji_biao$文理分科,
 
 library("cluster")
 imodel <- pam(cj, 2)
-min(Metrics::ce(cheng_ji_biao$文理分科, 
+min(Metrics::ce(cjb$wlfk, 
                 c( "理科", "文科")[imodel$cluster]),
-    1- Metrics::ce(cheng_ji_biao$文理分科, 
+    1- Metrics::ce(cjb$wlfk, 
                    c( "理科", "文科")[imodel$cluster]))
 #[1] 0.3225806 未剔除异常点
 #[1] 0.3215223 剔除异常点
 
-imodel <- pam(cj[, c("生物", "物理", "数学")], 2)
-min(Metrics::ce(cheng_ji_biao$文理分科, 
+imodel <- pam(cj[, c("sw", "wl", "sx")], 2)
+min(Metrics::ce(cjb$wlfk, 
                 c( "理科", "文科")[imodel$cluster]),
-    1- Metrics::ce(cheng_ji_biao$文理分科, 
+    1- Metrics::ce(cjb$wlfk, 
                    c( "理科", "文科")[imodel$cluster]))
 #[1] 0.3109677 未剔除异常点
 #[1] 0.3097113 剔除异常点
@@ -388,12 +518,13 @@ fviz_cluster(imodel)
 #######################################################
 ##层次聚类法
 #######################################################
-
+cj_dist <- dist(cjb %>%
+                  select(yw:zz))
 imodel_hc <- hclust(cj_dist, method = "ward.D")
 imodel_hc_cut2 <- cutree(imodel_hc, k = 2)
-(ic_metric <- min(Metrics::ce(cheng_ji_biao$文理分科, 
+(ic_metric <- min(Metrics::ce(cjb$wlfk, 
                              c( "理科", "文科")[imodel_hc_cut2]),
-                 1- Metrics::ce(cheng_ji_biao$文理分科, 
+                 1- Metrics::ce(cjb$wlfk, 
                                 c( "理科", "文科")[imodel_hc_cut2])))
 #[1] 0.2860892
 #分类准确率挺近70%大关
@@ -418,13 +549,13 @@ for (dist_method in c("euclidean",
     "centroid"
   )) {
     cat("Processing ", hc_method, "\t", dist_method, "\n")
-    imodel_hc <- hclust(stats::dist(cheng_ji_biao[,  4:12],
+    imodel_hc <- hclust(stats::dist(cjb[,  4:12],
                                     method = dist_method),
                         method = hc_method)
     imodel_hc_cut2 <- cutree(imodel_hc, k = 2)
-    hc_metric <- min(Metrics::ce(cheng_ji_biao$文理分科,
+    hc_metric <- min(Metrics::ce(cjb$wlfk,
                                  c("理科", "文科")[imodel_hc_cut2]),
-                     1 - Metrics::ce(cheng_ji_biao$文理分科,
+                     1 - Metrics::ce(cjb$wlfk,
                                      c("理科", "文科")[imodel_hc_cut2]))
     hc_metric_com <- rbind(
       hc_metric_com,
@@ -447,12 +578,12 @@ rect.hclust(imodel_hc, k = 2, border = 2:3)
 #层次聚类，画的更好看一点
 library(dendextend)
 dend_hc <- as.dendrogram(imodel_hc)
-dend <- rotate(dend_hc, 1:nrow(cheng_ji_biao))
+dend <- rotate(dend_hc, 1:nrow(cjb))
 # Color the branches based on the clusters:
 dend <- color_branches(dend, k = 2) 
 # Manually match the labels, as much as possible, to the real classification of the flowers:
 labels_colors(dend) <-
-  colorspace::rainbow_hcl(2)[sort_levels_values(as.numeric(cheng_ji_biao$文理分科)[order.dendrogram(dend)])]
+  colorspace::rainbow_hcl(2)[sort_levels_values(as.numeric(cjb$wlfk)[order.dendrogram(dend)])]
 #Hang the dendrogram a bit:
 dend <- hang.dendrogram(dend, hang_height = 0.1)
 dend <- set(dend, "labels_cex", 0.25)
@@ -463,61 +594,35 @@ plot(
   nodePar = list(cex = .007)
 )
 legend("topright",
-       legend = levels(cheng_ji_biao$文理分科),
+       legend = levels(cjb$wlfk),
        fill = colorspace::rainbow_hcl(2))
 
 
 #######################################################
-##dbscan算法
+##尝试一些算法创新
 #######################################################
-#先看一个演示
-#基于人工数据
-library(factoextra)
-data(multishapes)
-df <- multishapes[, 1:2]
-set.seed(2012)
-km_res <- kmeans(df, 5)
-fviz_cluster(km_res, df, ellipse = FALSE, geom = "point")
-set.seed(2012)
-db <- fpc::dbscan(df, eps = 0.15, MinPts = 5)
-fviz_cluster(db, df, ellipse = FALSE, geom = "point")
-
-dbscan::kNNdistplot(df, k =  5)
-abline(h = 0.15, lty = 2)
-
-#针对cheng_ji_biao数据集进行聚类
-#找到相应的k和Epsilon
-cj_matri <- as.matrix(cheng_ji_biao[, 4:12])
-dbscan::kNNdistplot(cj_matri, k =  18)
-abline(h = 10, lty = 2, col = "red")
-
-#利用fpc::dbscan()或dbscan::dbscan()扩展包进行聚类
-set.seed(2012)
-imodel_db <- fpc::dbscan(cj_matri,
-                         eps = 10,
-                         MinPts = 18)
-imodel_db$cluster
-(ic_metric <- min(
-  Metrics::ce(cheng_ji_biao$文理分科,
-              c("理科", "文科")[(imodel_db$cluster == 1) + 1]),
-  1 - Metrics::ce(cheng_ji_biao$文理分科,
-                  c("理科", "文科")[(imodel_db$cluster == 1) + 1])
-))
-#[1] 0.3293963
-
-#绘制聚类图形
-imodel_db <- list(data = cj_matri,
-                  cluster = imodel_db$cluster)
-fviz_cluster(imodel_db, 
-             cj_matri,
-             geom = "point")
-
-
-#从聚类效果可以看出，
-#基于密度的聚类，针对我们目前的问题情境，
-#其实并不适用，实际上，对于我们问题的情境
-#很难想象具有想前述人工数据那样的形状，
-#即便有，对于成绩数据而言，也很难解释。
+#了解完以上的基本原理之后，
+#小伙伴们也应该有算法创造者的角度，
+#对其开展研究
+library(DMwR)
+out_rank <- outliers.ranking(
+  dist(cjb[, 4:12]),
+  clus = list(dist = "euclidean",
+              alg = "hclust",
+              meth = "ward.D"
+              )
+  )
+cjb %>%
+  arrange(desc(out_rank$prob.outliers)) %>%
+  View()
+out_rank$prob.outliers
+cjb[order(out_rank$prob.outliers, decreasing = TRUE)[1:10], ] %>%
+  View()
+(outliers <- boxplot.stats(cjb$zcj)$out)
+#异常点的标签
+outliers_idx <- which(cjb$zcj %in% outliers)
+#显示异常点
+View(cjb[outliers_idx, ])
 
 
 #######################################################
