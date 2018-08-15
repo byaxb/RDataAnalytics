9373459874882cb80ecda86163dc7f72414b0635
byaxb <axb@bupt.edu.cn>
2018-08-15 13:59:40 +0800

    Update ch00_Mini Case.R

--- ch00_Mini Case.R
+++ ch00_Mini Case.R
@@ -1,10 +1,46 @@
+
+
+#######################################################
+#######################################################
+##
+## 名称：《R语言数据分析·Mini Case》
+## 作者：艾新波
+## 学校：北京邮电大学
+## 版本：V9
+## 时间：2018年8月
+##
+##*****************************************************
+##
+## ch00_Mini Case_V9
+## Data Analytics with R
+## Instructed by Xinbo Ai
+## Beijing University of Posts and Telecommunications
+##
+##*****************************************************
+##
+## Author: byaxb
+## Email:axb@bupt.edu.cn
+## QQ:23127789
+## WeChat:13641159546
+## URL:https://github.com/byaxb
+##
+##*****************************************************
+##
+## (c)2012~2018
+##
+#######################################################
+#######################################################
+
+
+
 #读取数据
 library(readxl)
+#请从github中下载cjb.xlsx文件
+#在当前工作路径创建data子文件夹并将该excel文件置于其中
 cjb <- readxl::read_excel("data/cjb.xlsx")
-View(cjb)
+View(cjb) #如果使用的是RStudio，将在代码面板中展示
 #对数据进行探索性分析
 library(tidyverse)
-x11()
 cjb %>%
   select(sx, wlfk) %>%
   ggplot(aes(
@@ -33,7 +69,11 @@ my_model <- cjb %>%
   select(xb:wlfk) %>%
   apriori(parameter = list(supp = 0.06, conf = 0.8), 
   appearance = list(rhs = paste0("wlfk=", c("文科", "理科"))))
-#模型评估
+#模型评估：看规则的支持度、置信度和提升度
 inspectDT(my_model)
 #可视化
 plot(my_model, method = "graph")
+#当然，也可采用交互的方式
+plot(my_model,
+     method = "graph",
+     engine = "htmlwidget")
