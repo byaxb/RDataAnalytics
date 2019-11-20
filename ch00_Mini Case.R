609692a670d9185535261cda3784a14609f306f3
byaxb <axb@bupt.edu.cn>
2019-11-20 22:25:47 +0800

    a tidy version

--- ch00_Mini Case.R
+++ ch00_Mini Case.R
@@ -1,4 +1,7 @@
 
+# ch00_Mini Case ----------------------------------------------------------
+
+
 # Data Import -------------------------------------------------------------
 
 library(readxl)
@@ -14,28 +17,29 @@ View(cjb)
 
 library(tidyverse)
 cjb %>%
-  select(sx, wlfk) %>%
-  ggplot(aes(
-    x = wlfk,
-    y = sx,
-    fill = wlfk)) +
-  geom_boxplot(width = 0.5)
+    select(sx, wlfk) %>%
+    ggplot(aes(x = wlfk,
+               y = sx,
+               fill = wlfk)) +
+    geom_boxplot(width = 0.5)
 
 
 # Data Preparation --------------------------------------------------------
 as_five_grade_scores <- function(x) {
-  cut(x,
-      breaks = c(0, seq(60, 100, by = 10)),
-      include.lowest = TRUE,
-      right = FALSE,
-      ordered_result = TRUE,
-      labels = c("不及格", "及格", "中", "良", "优"))
+    cut(
+        x,
+        breaks = c(0, seq(60, 100, by = 10)),
+        include.lowest = TRUE,
+        right = FALSE,
+        ordered_result = TRUE,
+        labels = c("不及格", "及格", "中", "良", "优")
+    )
 }
 cjb <- cjb %>%
-  mutate(zcj = rowSums(.[4:12])) %>%
-  filter(zcj != 0) %>% #剔除脏数据
-  mutate_at(vars(xb, wlfk), factor) %>% #类型转换
-  mutate_at(vars(yw:sw), as_five_grade_scores)#数据分箱
+    mutate(zcj = rowSums(.[4:12])) %>%
+    filter(zcj != 0) %>% #剔除脏数据
+    mutate_at(vars(xb, wlfk), factor) %>% #类型转换
+    mutate_at(vars(yw:sw), as_five_grade_scores)#数据分箱
 View(cjb)
 
 
@@ -43,9 +47,9 @@ View(cjb)
 
 library(arulesViz)
 my_model <- cjb %>%
-  select(xb:wlfk) %>%
-  apriori(parameter = list(supp = 0.06, conf = 0.8),
-  appearance = list(rhs = paste0("wlfk=", c("文科", "理科"))))
+    select(xb:wlfk) %>%
+    apriori(parameter = list(supp = 0.06, conf = 0.8),
+            appearance = list(rhs = paste0("wlfk=", c("文科", "理科"))))
 
 
 # Visualization -----------------------------------------------------------
@@ -55,3 +59,8 @@ plot(my_model, method = "graph")
 plot(my_model,
      method = "graph",
      engine = "htmlwidget")
+
+
+# The End ^-^ -------------------------------------------------------------
+
+
